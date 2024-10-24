f07f19a134de51d551a9d5d1750b523b9bc61d1e
mesafrancisco <145351124+mesafrancisco@users.noreply.github.com>
2024-10-24 21:09:29 +0200

    Introduccion de proyecto spring

---
+++ com.app.incidencia/src/test/java/com/app/incidencia/com/app/incidencia/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.incidencia.com.app.incidencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
