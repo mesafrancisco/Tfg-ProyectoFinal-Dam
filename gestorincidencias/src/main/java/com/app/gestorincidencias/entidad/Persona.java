d2197618f0c965c5446bef96591745b95adb6c7c
mesafrancisco <145351124+mesafrancisco@users.noreply.github.com>
2024-10-24 21:42:21 +0200

    Creacion persona

--- gestorincidencias/src/main/java/com/app/gestorincidencias/entidad/Persona.java
+++ gestorincidencias/src/main/java/com/app/gestorincidencias/entidad/Persona.java
@@ -8,4 +8,61 @@ public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nombre", nullable = false, length = 50)
+    private String nombre;
+
+    @Column(name = "apellido", nullable = false, length = 50)
+    private String apellido;
+
+    @Column(name = "email", nullable = false, length = 50, unique = true)
+    private String email;
+
+    public Persona() {
+    }
+
+    public Persona(Long id, String email, String apellido, String nombre) {
+        this.id = id;
+        this.email = email;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public Persona(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
