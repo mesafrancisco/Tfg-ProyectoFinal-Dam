380342bf883f6952cfc188fcee5fd5fedc082dbf
mesafrancisco <145351124+mesafrancisco@users.noreply.github.com>
2024-12-07 13:13:26 +0100

    a

--- gestorincidencias/src/main/java/com/app/gestorincidencias/controlador/RegistroUsuarioControlador.java
+++ gestorincidencias/src/main/java/com/app/gestorincidencias/controlador/RegistroUsuarioControlador.java
@@ -32,6 +32,6 @@ public class RegistroUsuarioControlador {
     @PostMapping
     public String registrarCuentaDeUsuario(@ModelAttribute("usuario") UsuarioRegistroDTO registroDTO) {
         usuarioServicio.guardar(registroDTO);
-        return "redirect:/registro?exito";
+        return "redirect:/incidencias";
     }
 }
