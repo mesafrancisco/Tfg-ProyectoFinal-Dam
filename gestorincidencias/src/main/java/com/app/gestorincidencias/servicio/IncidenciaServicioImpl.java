856ab98c5ad4880aecd304641d2e28c3b86ec965
CalvoManuelCalzado <Manuel-Calvo1@eep-igroup.com>
2025-01-13 19:45:27 +0100

    feat(incidencias): mejora en el filtro y aplicación del paginado en el listado

--- gestorincidencias/src/main/java/com/app/gestorincidencias/servicio/IncidenciaServicioImpl.java
+++ gestorincidencias/src/main/java/com/app/gestorincidencias/servicio/IncidenciaServicioImpl.java
@@ -3,9 +3,10 @@ package com.app.gestorincidencias.servicio;
 import com.app.gestorincidencias.entidad.Incidencia;
 import com.app.gestorincidencias.repositorio.IncidenciaRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class IncidenciaServicioImpl implements IncidenciaServicio {
@@ -14,11 +15,18 @@ public class IncidenciaServicioImpl implements IncidenciaServicio {
     private IncidenciaRepositorio repositorio;
 
     @Override
-    public List<Incidencia> listarTodasLasIncidencias(String palabraClave) {
+    public Page<Incidencia> listarTodasLasIncidencias(String palabraClave, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
         if (palabraClave != null && !palabraClave.isEmpty()) {
-            return repositorio.findAllByPalabraClave(palabraClave);
+            return repositorio.findAllByPalabraClave(palabraClave, pageable);
         }
-        return repositorio.findAll(); // Esto es de Jpa
+        return repositorio.findAll(pageable); // Si no hay palabra clave, devuelve todas las incidencias con paginación
+    }
+
+    @Override
+    public Page<Incidencia> buscarPorFiltros(String titulo, String estado, String descripcion, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return repositorio.findByFiltros(titulo, estado, descripcion, pageable); // Devuelve las incidencias con filtros aplicados
     }
 
     @Override
